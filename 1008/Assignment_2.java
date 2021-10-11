e957b9246d93fb3f1014b4045f2d7b444e3a6ace
김인회 <90288195+inhoekim@users.noreply.github.com>
2021-10-11 19:26:44 +0900

    10월 8일 과제 (2)
    프로그램을 실행하면 test.txt파일의 모든 내용을 대문자로 변환해 upper_test.txt파일에 저장되도록 만들어 보세요
     ## test.txt
     hello!
     my name is hong gil dong.
    
     ==> 프로그램 실행후 upper_test.txt파일에 아래처럼 저장된다.
     HELLO!
     MY NAME IS HONG GIL DONG.

--- 1008/Assignment_2.java
+++ 1008/Assignment_2.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Final2 {
+public class Assignment_2 {
 	public static void main(String[] args) {
 		FileReader fr = null;
 		FileWriter fw = null;
