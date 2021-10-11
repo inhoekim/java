7749489713c477d73b5fc4acf2790dd5110c22bb
김인회 <90288195+inhoekim@users.noreply.github.com>
2021-10-11 19:26:15 +0900

    10월 8일 과제 (1)
    영문으로 키보드로 문자열을 입력받아 test.txt파일로 저장하는 프로그램을 작성해 보세요.

--- 1008/Assignment_1.java
+++ 1008/Assignment_1.java
@@ -2,6 +2,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
+
 public class Assignment_1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
