d91a507a844d799aa6dc814a294fb299f067b4df
김인회 <90288195+inhoekim@users.noreply.github.com>
2021-11-12 08:27:12 +0900

    멀티 채팅방 프로그램
    유동적인 방 추가 및 삭제 기능은 미구현

---
+++ 1112/MultiChatClient.java
@@ -0,0 +1,83 @@
+package test04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+
+class RecThread extends Thread{
+    Socket socket;
+    public RecThread(Socket socket) {
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while(true) {
+                String msg = br.readLine();
+                if (msg == null) {
+                    System.out.println("<서버가 종료되었습니다>");
+                    break;
+                }
+                System.out.println(msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class SendThread extends Thread{
+    Socket socket;
+
+    public SendThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            System.out.println("<접속할 채팅방을 선택하세요");
+            System.out.println("1.1번방  2.2번방  3.3번방");
+            String roomNum = sc.nextLine();
+            pw.println(roomNum);
+            System.out.println("<사용하실 닉네임을 입력하세요>");
+            String userName = sc.nextLine();
+            pw.println("<" + userName + "님이 채팅방에 접속하셨습니다>");
+            while(true) {
+                String msg = sc.nextLine();
+                if(msg.equals("exit")) {
+                    pw.println("<"+ userName + "님이 채팅방을 나가셨습니다>");
+                    break;
+                }
+                pw.println(userName + ">>" + msg);
+            }
+            pw.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class MultiChatClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 3000);
+            System.out.println("<서버에 접속되었습니다>");
+            new RecThread(socket).start();
+            new SendThread(socket).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ 1112/MultiChatServer.java
@@ -0,0 +1,81 @@
+package test04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+
+class EchoThread extends Thread{
+    private Socket socket = null;
+    private Vector<Vector<Socket>> roomList = null;
+    private int roomNum;
+
+    public EchoThread(Socket socket, Vector<Vector<Socket>> roomList) {
+        this.socket = socket;
+        this.roomList = roomList;
+    }
+
+    public void sendMsg(String msg) {
+        for (Socket s : roomList.get(roomNum)) {
+            if (socket == s) continue;
+            try {
+                PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
+                pw.println(msg);
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.roomNum = Integer.parseInt(br.readLine()) - 1;
+            roomList.get(roomNum).add(socket);
+            while(true) {
+                String msg = br.readLine();
+                if(msg == null) {
+                    System.out.println("<클라이언트(" + socket + ")가 연결을 종료하였습니다>");
+                    roomList.get(roomNum).remove(socket);
+                    break;
+                }
+                sendMsg(msg);
+            }
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class MultiChatServer {
+    public static Vector<Vector<Socket>> roomList = new Vector<>();
+    public static Vector<Socket> room1 = new Vector<>();
+    public static Vector<Socket> room2 = new Vector<>();
+    public static Vector<Socket> room3 = new Vector<>();
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            roomList.add(room1);
+            roomList.add(room2);
+            roomList.add(room3);
+        while(true) {
+                System.out.println("<서버 대기중..>");
+                Socket socket = server.accept();
+                System.out.println("<새로운 클라이언트(" + socket + ")가 접속하였습니다>");
+                new EchoThread(socket, roomList).start();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
